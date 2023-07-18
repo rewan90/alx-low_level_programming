e0bb10ff6efc971f484484b426d024d7a03baf2f
Rewan <rewan.noufal.1998@gmail.com>
2023-07-18 15:53:21 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - point
+ * Return:0
+ */
+
+int main(void)
+{
+char c[5] = "_putchar";
+int i = 0;
+while (i < 4)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
