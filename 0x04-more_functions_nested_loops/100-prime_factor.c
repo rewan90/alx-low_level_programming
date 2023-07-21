9cda1f614ea5ca49b15d3a475e5d953f18ecbb77
Rewan <rewan.noufal.1998@gmail.com>
2023-07-20 18:30:15 -0700

    add advanced tasks

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prime numbers
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int x = 612852475143;
+	long int p;
+
+	for (p = 2; p < x; p++)
+	{
+		if (x % p == 0)
+		{
+			x = x / p;
+		}
+	}
+	printf("%ld\n", p);
+	return (0);
+}
